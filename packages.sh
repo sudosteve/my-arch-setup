b0d7e94bc74d8ca4e7637222b828c6b7bd447707
Stephen Thomas-Dorin <stephen.thomasdorin@gmail.com>
2020-01-13 06:11:09 +0000

    fixed typo

--- packages.sh
+++ packages.sh
@@ -5,5 +5,5 @@ pacn () { sudo pacman --noconfirm --needed -S $@ ; }
 
 pacn dialog wpa_supplicant
 pacn xorg-server xorg-xinit 
-pacn i3-gaps dmenu i3-status noto-fonts
+pacn i3-gaps dmenu i3status noto-fonts
 pacn terminator htop chromium
