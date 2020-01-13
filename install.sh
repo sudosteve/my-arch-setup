39319afa0348ac5fd099da355a3a53b126ef2612
Stephen Thomas-dorin <stephen.thomasdorin@gmail.com>
2020-01-13 01:31:19 +0000

    installer script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Run this after the drive has been properly partitioned
+
+source params.sh
+
+mount $ROOT /mnt
+swapon $SWAP
+mkdir /mnt/boot/EFI
+mount $EFI /mnt/boot/EFI
+
+pacstrap /mnt base base-devel linux linux-firmware
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+
+ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
+hwclock --systohc
+vim /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo $HOSTNAME > /etc/hostname
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "::1             localhost" >> /etc/hosts
+echo "127.0.1.1       $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
+
+passwd
+
+pacman --noconfirm --needed -S networkmanager dhcpcd
+systemctl enable NetworkManager
+
+pacman --noconfirm --needed -S grub os-prober
+grub-install $DRIVE
+
+exit
+umount -a
+reboot
+
