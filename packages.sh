f44c211e6b8e50da20a290df98508edfb1780bd9
Stephen Thomas-Dorin <stephen.thomasdorin@gmail.com>
2020-01-14 23:41:55 +0000

    oh-my-zsh and dotfiles

--- packages.sh
+++ packages.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-alias pacn="sudo pacman --noconfirm --needed -S"
+# pacman packages
+
 pacn () { sudo pacman --noconfirm --needed -S $@ ; }
 
 pacn dialog wpa_supplicant
-pacn xorg-server xorg-xinit 
-pacn i3-gaps dmenu i3status noto-fonts
-pacn terminator htop chromium
+pacn xorg-server xorg-xinit xorg-xsetroot
+pacn alsa-utils tlp
+pacn i3-gaps dmenu network-manager-applet i3status noto-fonts feh
+pacn htop openssh man wget curl
+pacn qutebrowser chromium rxvt-unicode
+
+# install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+
+# yay packages
+
+yay -S pywal-git
+yay -S dina-font-ttf
+
--- user_setup.sh
+++ user_setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+source params.sh
+
 # install all packages
 ./packages.sh
 
 sudo cp xorgconf/* /etc/X11/xorg.conf.d/
+sudo gpasswd -a $USER bumblebee
+
+CHSH="no" RUNZSH="no" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# install dots
+cp -r dots/.* ~/
 
