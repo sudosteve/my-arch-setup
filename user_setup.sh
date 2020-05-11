bed68ff1e5210e6db4740f56925725028f2fcf25
Stephen Thomas-Dorin <stephen.thomasdorin@gmail.com>
2020-05-11 18:17:49 -0400

    new user_setup stuff

--- user_setup.sh
+++ user_setup.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-source params.sh
+# make sure we are in right directory
+[ -f user_setup.sh ] || (echo "RUN FROM REPO ROOT PLS" && exit)
 
-# install all packages
-./packages.sh
+# install pacman packages
+sudo pacman -S - < pacmanlist.txt
 
+# install xorg confs
+# - disables mouse acceleration
+# - enables trackpad touch to click and natural scrolling
 sudo cp xorgconf/* /etc/X11/xorg.conf.d/
-sudo gpasswd -a $USER bumblebee
 
-CHSH="no" RUNZSH="no" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install yay packages
+yay -S - < aurlist.txt
+
+cd ..
+
+# install yay AUR helper
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
 
 
 # install dots
-cp -r dots/.* ~/
+git clone git@github.com:sk8ersteve/dotfiles.git
+cd ~
+ln -s git-repos/dotfiles/.config .config
+ln -s git-repos/dotfiles/.local .local
+ln -s git-repos/dotfiles/.profile .profile
+ln -s git-repos/dotfiles/.profile .zprofile
+mkdir -p .local/share/zsh
+
+# install oh my zsh
+CHSH="no" RUNZSH="no" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
