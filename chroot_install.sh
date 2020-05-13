fd847b3a8a1bce2d376d16fb488c060a703b800a
Stephen Thomas-Dorin <stephen.thomasdorin@gmail.com>
2020-05-13 19:59:53 -0400

    added dialogs and easier download

--- chroot_install.sh
+++ chroot_install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Prompt user for hostname, username
+HOSTNAME=$(dialog --stdout --inputbox "Enter your hostname" 10 50 --stdout)
+USERNAME=$(dialog --stdout --inputbox "Enter your username" 10 50 --stdout)
+clear
+
+# locale setting
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
+# hostname settings
 echo $HOSTNAME > /etc/hostname
 echo "127.0.0.1       localhost" >> /etc/hosts
 echo "::1             localhost" >> /etc/hosts
@@ -16,29 +23,27 @@ passwd
 
 # Network Manager
 pacman --noconfirm --needed -S networkmanager
-# Try without dhcpcd because I'm still not sure what arch installs by default
-# pacman --noconfirm --needed -S networkmanager dhcpcd
+pacman --noconfirm --needed -S networkmanager dhcpcd
 systemctl enable NetworkManager
-# systemctl enable dhcpcd
+systemctl enable dhcpcd
 
 # add user with zsh
 pacman --noconfirm --needed -S zsh
-useradd -m -G wheel -s /bin/zsh $USER
-echo "Set password for user $USER"
-passwd $USER
-pacman -S sudo
-visudo
+useradd -m -G wheel -s /bin/zsh $USERNAME
+echo "Set password for user $USERNAME"
+passwd $USERNAME
+
+# add perissions to sudoers file
+echo "%wheel ALL=(ALL) ALL
+%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman,/usr/bin/yay" >> /etc/sudoers
 
 # graphics
-pacman --noconfirm --needed -S xf86-video-intel bumblebee nvidia bbswitch
-systemctl enable bumblebeed.service
+pacman --noconfirm --needed -S xf86-video-intel nvidia bbswitch
 
 # install grub bootloader
-pacman --noconfirm --needed -S grub os-prober
-grub-install $DRIVE
+pacman --noconfirm --needed -S grub os-prober efibootmgr
+grub-install $DISK
 grub-mkconfig -o /boot/grub/grub.cfg
 
 exit
-umount -a
-reboot
 
--- install.sh
+++ install.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
 
-# Run this after the drive has been properly partitioned
+pacman -Sy --noconfirm dialog || { echo "Not running as root" ; exit; }
 
-source params.sh
+DISKS="$(lsblk -rpo "name,size,type,mountpoint" | awk '$3=="disk"{printf "%s (%s)\n", $1, $2}')"
 
+# Pick a disk for installation
+DISK="$(dialog --stdout --title "Choose for installation" --menu "Select which disk you would like to use for the installation" 10 70 4 $(echo $DISKS))"
+
+# Ask if we want to partition a disk
+dialog --defaultno --title "Partion disk?" --yesno "Would you like to open fdisk to partition the disk" 8 50 && ( clear ; fdisk $DISK )
+
+# unmount partitions, then pick partitions for installation
+umount -a
+getparts() { \
+    lsblk -rpo "name,size,type,parttypename,mountpoint" | grep $1 | \
+    awk '$3=="part"&&$5==""{printf "%s (%s)\n",$1,$2}'
+}
+getpart() { \
+    [ -z "$(getparts $3)" ] || dialog --stdout --title "$1" \
+    --menu "$2" 16 70 8 $(getparts $3)
+}
+ROOT=$(getpart "Root partition" "Choose a partition to install Arch on" "filesystem")
+[ -z $ROOT ] && clear && echo "No filesystem partitions found for root" && exit
+SWAP=$(getpart "Swap partition" "Choose swap partition" "swap")
+[ -z $SWAP ] && clear && echo "No swap partitions found" && exit
+EFI=$(getpart "efi partition" "Choose efi system partition" "EFI")
+[ -z $EFI ] && clear && echo "No efi partitions found" && exit
+
+# start installation
 mount $ROOT /mnt
 swapon $SWAP
 
 pacstrap /mnt base base-devel linux linux-firmware vim
 
-mkdir /mnt/boot/EFI
-mount $EFI /mnt/boot/EFI
+mkdir /mnt/boot/efi
+mount $EFI /mnt/boot/efi
 genfstab -U /mnt >> /mnt/etc/fstab
 
+wget https://raw.githubusercontent.com/sk8ersteve/my-arch-setup/master/chroot_install.sh
+chmod +x chroot_install.sh
 cp chroot_install.sh /mnt/install.sh
 arch-chroot /mnt ./install.sh
 rm /mnt/install.sh
